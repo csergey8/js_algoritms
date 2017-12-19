6939ae417bcd7ad856b8c043b24973c1f94568fa
csergey8 <c.sergey@protonmail.com>
2017-12-19 09:36:24 +0200

    completed revers word function

---
+++ revers_words.js
@@ -0,0 +1,14 @@
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+
+  wordsArr.forEach( word => {
+    var reveresedWord = '';
+    for (var i = word.lengthl i >=0; i--){
+      reveresedWord += word[i];
+    }
+    reversedWordsArr.push(reveresedWord);
+  });
+
+  return reversedWordsArr.join(' ');
+}
